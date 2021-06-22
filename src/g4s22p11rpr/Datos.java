916ea5331a220cad600c1dcf2bd9bcda1b90bfa7
ABIGAILSOFIA <ABIGAILSOFIA@Sofy-Luis>
2021-06-22 00:03:22 -0500

    Practica 11

---
+++ src/g4s22p11rpr/Datos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s22p11rpr;
+
+/**
+ *
+ * @author ABIGAILSOFIA
+ */
+public class Datos {
+    String Nombre;
+    int Edad;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+}
